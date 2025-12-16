8c69ad1f61fc10dba44e487e4ef185296ab3c714
Valentina1891 <valentina.valdivia.me@gmail.com>
2025-12-15 23:30:05 -0300

    Rename admin folder

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,8 +7,8 @@ import Login from "./pages/Login";
 import Register from "./pages/Registro";      // ← NUEVA
 import MenuPublico from "./pages/MenuPublico";
 import MisReservas from "./pages/MisReservas";
-import AdminComidas from "./pages/admin/AdminComidas";
-import AdminReservas from "./pages/admin/AdminReservas";
+import AdminComidas from "./pages/Admin/AdminComidas";
+import AdminReservas from "./pages/Admin/AdminReservas";
 import Forbidden from "./pages/Forbidden";
 import Footer from "./components/Footer";
 
@@ -17,14 +17,16 @@ export default function App(){
     <>
       <NavBar/>
       <Routes>
-        <Route index element={<Home/>}/>
+        <Route path="/" element={<Home/>} />
         <Route path="/menu" element={<MenuPublico/>}/>
         <Route path="/login" element={<Login/>}/>
-        <Route path="/register" element={<Register/>}/> {/* ruta nueva */}
+        <Route path="/register" element={<Register/>}/>
         <Route path="/403" element={<Forbidden/>}/>
         <Route path="/reservas" element={<RequireAuth><MisReservas/></RequireAuth>}/>
         <Route path="/admin/comidas" element={<RequireRole role="admin"><AdminComidas/></RequireRole>}/>
         <Route path="/admin/reservas" element={<RequireRole role="admin"><AdminReservas/></RequireRole>}/>
+        {/* fallback */}
+        <Route path="*" element={<Home/>} />
       </Routes>
       <Footer></Footer>
     </>
